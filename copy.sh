c866a4e3aa2820b17a6b85723ca660ffd0b92e73
Ali Pirani <alipirani88@gmail.com>
2019-03-05 17:22:42 -0500

    Adding IGV screenshots to website

---
+++ copy.sh
@@ -0,0 +1,6 @@
+cp day1_morning/README.md docs/source/day1_morning.md 
+cp day1_afternoon/README.md docs/source/day1_afternoon.md 
+cp day2_morning/README.md docs/source/day2_morning.md 
+cp day2_afternoon/README.md docs/source/day2_afternoon.md 
+cp day3_morning/README.md docs/source/day3_morning.md 
+cp day3_afternoon/README.md docs/source/day3_afternoon.md
